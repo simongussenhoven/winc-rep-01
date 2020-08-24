0b2414e40cb9c5ba8778928cc4fe98405cb79721
simongussenhoven <69677504+simongussenhoven@users.noreply.github.com>
2020-08-24 19:59:49 +0200

    Javascript 1

---
+++ Assignment-20/script.js
@@ -0,0 +1,12 @@
+let name = "Simon";
+let ifSo = "Dit kan wel";
+const sumOfSomething = 4 + 4;
+const letsMultiply = 2 * 256;
+const division = 2 / 3;
+const age = 48;
+
+console.log("Hello Winc Academy");
+console.log(name);
+console.log(sumOfSomething);
+console.log(letsMultiply);
+console.log(division);
