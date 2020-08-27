8a3a385e727aca0b4b331a21c48f1aa0fc5840df
simongussenhoven <69677504+simongussenhoven@users.noreply.github.com>
2020-08-27 18:21:47 +0200

    Meer if else

---
+++ Assignment-23/script.js
@@ -0,0 +1,34 @@
+if (age > 17 && age < 26) {
+    console.log('Je krijgt 50% korting!')
+}
+else {
+    console.log('Volle prijs!')
+}
+
+const name = "Frits";
+
+if (name == "Bram" || name == "Sarah") {
+    console.log('Gratis bier');
+}
+else {
+    console.log('Betalen');
+}
+
+const totalAmount = 52;
+
+if (totalAmount > 25) {
+    if (totalAmount > 50) {
+        if (totalAmount > 99) {
+            console.log('Je krijgt champagne')
+        }
+        else {
+            console.log('Je krijgt nachos');
+        }
+    }
+    else {
+        console.log('Je krijgt vegaballetjes');
+    }
+}
+else {
+    console.log('Je krijgt niks');
+}
