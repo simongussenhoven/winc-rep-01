14904d10689b8116456ea1fc21dde491fbd011db
simongussenhoven <69677504+simongussenhoven@users.noreply.github.com>
2020-09-01 21:09:30 +0200

    Update script.js

--- Assignment-30/script.js
+++ Assignment-30/script.js
@@ -1,20 +1,21 @@
 let userName = prompt('Wat is je naam?');
 let min = prompt('Vanaf welk nummer wil je raden?');
 let max = prompt('Tot welk nummer wil je raden?');
-let tries = 5;
+let tries = 4;
 let random = Math.floor(Math.random() * (max - min + 1) + min);
 
 console.log(userName + ', ' + min + ', ' + max)
-alert('Hallo ' + userName + '. Je raadt tussen ' + min + ' en ' + max + '. Succes!')
+alert('Hallo ' + userName + '. Je raadt tussen ' + min + ' en ' + max + ', je hebt 5 pogingen. Succes!')
 
-for (tries = 5; tries > 0; --tries) {
+for (tries = 4; tries > -1;) {
     userNumber = prompt('Doe een gok.')
     if (userNumber == random) {
         alert('Dat is goed! Je hebt gewonnen.');
         break;
     }
     else {
-        alert('Dat was fout. Je hebt nog ' + tries + ' pogingen.')
+        alert('Dat was fout. Je hebt nog ' + tries + ' pogingen over.');
+        --tries;
     }
 }
-alert('Je hebt geen pogingen meer over. Het spel is nu voorbij.')
+alert('Het spel is nu voorbij.')
