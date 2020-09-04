402f59683f026434f6aebaa5a260e9f0e1255c4d
simongussenhoven <69677504+simongussenhoven@users.noreply.github.com>
2020-09-04 16:07:44 +0200

    Sidemenu colorswitcher

---
+++ Assignment-32/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let hoverButton = document.getElementById('hoverbutton');
+    let sideMenu = document.getElementById('sidemenu');
+    let radios = document.getElementById('colorform');
+    let container = document.getElementById('container');
+    let colorExpression = document.getElementById('colorexpression');
+    let input = document.querySelector('input');
+
+    hoverButton.addEventListener('click', function () {
+        sideMenu.className = 'visible';
+    });
+
+    radios.addEventListener('click', function () {
+        let colors = document.getElementsByName('color');
+        let selectedColor;
+        for (i = 0; i < colors.length; i++) {
+            if (colors[i].checked) {
+                selectedColor = colors[i].value;
+                container.className = selectedColor;
+                sideMenu.className = 'hidden';
+                colorExpression.textContent = selectedColor;
+            }
+        }
+    });
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key === '1') {
+            container.className = 'green';
+        }
+        if (event.key === '2') {
+            container.className = 'red';
+        }
+        if (event.key === '3') {
+            container.className = 'yellow';
+        }
+    });
+});
