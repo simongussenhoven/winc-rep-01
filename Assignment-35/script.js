45b2698f76b433f38c55912bd51b6723a39202bd
Simon Gussenhoven <simongussenhoven@gmail.com>
2020-09-04 22:16:07 +0200

    Arrow functions

---
+++ Assignment-35/script.js
@@ -0,0 +1,24 @@
+//A
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+
+ikRockArrowFunctions()
+
+//B
+const fivePlusSeven = () => 5 + 7
+
+console.log(fivePlusSeven());
+
+//C 
+() => 1 + 2
+
+//D
+const myFunction = (a, b) => a + b
+console.log(myFunction(5, 6))
+
+//E
+const myFunction2 = addFive = a => a + 5
+console.log(myFunction2(5));
+
+//F
+const myFunction3 = createObject = () => ({ greeting: "hoi" })
+console.log(myFunction3())
