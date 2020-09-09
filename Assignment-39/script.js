a82643680ae00228a7ee4d50f2728d9ad0c67b99
Simon Gussenhoven <simongussenhoven@gmail.com>
2020-09-09 20:01:54 +0200

    Even more array methods

---
+++ Assignment-39/script.js
@@ -0,0 +1,156 @@
+const superheroes = [
+    {
+        "name": "Batman",
+        "publisher": "DC Comics",
+        "alter_ego": "Bruce Wayne",
+        "first_appearance": "Detective Comics #27",
+        "weight": "210"
+    },
+    {
+        "name": "Superman",
+        "publisher": "DC Comics",
+        "alter_ego": "Kal-El",
+        "first_appearance": "Action Comics #1",
+        "weight": "220"
+    },
+    {
+        "name": "Flash",
+        "publisher": "DC Comics",
+        "alter_ego": "Jay Garrick",
+        "first_appearance": "Flash Comics #1",
+        "weight": "195"
+    },
+    {
+        "name": "Green Lantern",
+        "publisher": "DC Comics",
+        "alter_ego": "Alan Scott",
+        "first_appearance": "All-American Comics #16",
+        "weight": "186"
+    },
+    {
+        "name": "Green Arrow",
+        "publisher": "DC Comics",
+        "alter_ego": "Oliver Queen",
+        "first_appearance": "All-American Comics #16",
+        "weight": "195"
+    },
+    {
+        "name": "Wonder Woman",
+        "publisher": "DC Comics",
+        "alter_ego": "Princess Diana",
+        "first_appearance": "The Incredible Hulk #180",
+        "weight": "165"
+    },
+    {
+        "name": "Blue Beetle",
+        "publisher": "DC Comics",
+        "alter_ego": "Dan Garret",
+        "first_appearance": "Mystery Men Comics #1",
+        "weight": "145"
+    },
+    {
+        "name": "Spider Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Peter Parker",
+        "first_appearance": "Amazing Fantasy #15",
+        "weight": "167"
+    },
+    {
+        "name": "Captain America",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Steve Rogers",
+        "first_appearance": "Captain America Comics #1",
+        "weight": "220"
+    },
+    {
+        "name": "Iron Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Tony Stark",
+        "first_appearance": "Tales of Suspense #39",
+        "weight": "250"
+    },
+    {
+        "name": "Thor",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Thor Odinson",
+        "first_appearance": "Journey into Myster #83",
+        "weight": "200"
+    },
+    {
+        "name": "Hulk",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Bruce Banner",
+        "first_appearance": "The Incredible Hulk #1",
+        "weight": "1400"
+    },
+    {
+        "name": "Wolverine",
+        "publisher": "Marvel Comics",
+        "alter_ego": "James Howlett",
+        "first_appearance": "The Incredible Hulk #180",
+        "weight": "200"
+    },
+    {
+        "name": "Daredevil",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Matthew Michael Murdock",
+        "first_appearance": "Daredevil #1",
+        "weight": "200"
+    },
+    {
+        "name": "Silver Surfer",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Norrin Radd",
+        "first_appearance": "The Fantastic Four #48",
+        "weight": "unknown"
+    }
+]
+
+// 1
+
+const heroNames = superheroes.map(hero => {
+    return hero.name;
+});
+
+// 2
+const lightHeroes = superheroes.filter(function (hero) {
+    return hero.weight < 190
+})
+
+// 3
+const heavyHeroes = superheroes.filter(function (hero) {
+    return hero.weight == 200
+})
+
+// 4
+const firstAppearance = superheroes.map(hero => {
+    return hero.first_appearance;
+})
+
+//5
+const fromDc = superheroes.filter(function (hero) {
+    return hero.publisher == "DC Comics"
+})
+
+const fromMarvel = superheroes.filter(function (hero) {
+    return hero.publisher == "Marvel Comics"
+})
+
+//7
+const weightOfDcHeroes = fromDc.map(hero => {
+    if (hero.weight !== "unknown") {
+        return parseInt(hero.weight, 10);
+    }
+    else {
+        return 0
+    }
+}).reduce((acc, curr) => acc + curr);
+
+const weightOfAllHeroes = superheroes.map(hero => {
+    if (hero.weight !== "unknown") {
+        return parseInt(hero.weight, 10);
+    }
+    else {
+        return 0
+    }
+}).reduce((acc, curr) => acc + curr);
