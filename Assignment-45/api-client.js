65a6fc58ea14e19e44097150c9279dc914957f82
Simon Gussenhoven <simongussenhoven@gmail.com>
2020-11-08 21:25:36 +0100

    Api interaction

--- Assignment-45/api-client.js
+++ Assignment-45/api-client.js
@@ -1,17 +1,15 @@
-async function getData() {
-    const API_KEY = "16a029c5dcc1795794a9c728a021c3ce"
-    const keySlot = "?api_key="
-    const apiUrl = 'https://api.themoviedb.org/3/genre/movie/list'
+
+const API_KEY = "?api_key=16a029c5dcc1795794a9c728a021c3ce"
+const baseUrl = 'https://api.themoviedb.org/'
+
+const getData = async (addedUrl) => {
     try {
-        const result = await
-            fetch(apiUrl + keySlot + API_KEY, { method: 'GET' })
-                .then(response => response.json();
-                console.log('Success')
-                )
-                .then()
-                .catch('error!')
+        const result = await fetch(baseUrl + addedUrl, { method: 'GET' })
+            .then(response => response.json())
+            .catch(error => console.log(error + 'error!'))
+        return result
     }
     catch (error) {
-        console.log("Catch: " + error);
+        console.log(error + 'Connection error!');
     }
 }
